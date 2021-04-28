984e83ea60480cd0fd7025d935b228d0dc0232dc
DallesLee <dalles.lee@gmail.com>
2021-04-28 21:32:48 +0200

    fix prune

--- src/transformers/modeling_bert.py
+++ src/transformers/modeling_bert.py
@@ -326,14 +326,15 @@ class BertAttention(nn.Module):
             self.self.key = prune_linear_layer(self.self.key, index)
             self.self.value = prune_linear_layer(self.self.value, index)
             self.output.dense = prune_linear_layer(self.output.dense, index, dim=1)
-        else:
-            self.self = None
-            self.output = None
 
         # Update hyper params and store pruned heads
         self.self.num_attention_heads = self.self.num_attention_heads - len(heads)
         self.self.all_head_size = self.self.attention_head_size * self.self.num_attention_heads
         self.pruned_heads = self.pruned_heads.union(heads)
+        
+        else:
+            self.self = None
+            self.output = None
 
     def forward(
         self,
